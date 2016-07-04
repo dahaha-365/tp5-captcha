3dba6066b78e2c680b0a76bf9eae42ce944f4cd1
Bolo Lai <cantonbolo@gmail.com>
2016-07-04 21:32:32 +0800

    改进

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tp5-captcha
-captcha package for thinkphp5, base on gregwar/captcha
+captcha package for ThinkPHP 5, base on `gregwar/captcha`
+基于`gregwar/captcha`的ThinkPHP 5验证码扩展
--- src/Captcha.php
+++ src/Captcha.php
@@ -2,14 +2,26 @@
 namespace tp5\captcha;
 
 use Gregwar\Captcha\CaptchaBuilder;
+load_trait('controller/Jump');
 
 class Captcha {
-    public function index() {
+    use \traits\controller\Jump;
+
+    public function index($phrase = null) {
+        if($phrase !== null) {
+            $valid = captcha_check($phrase);
+            if(request()->isAjax()) {
+                return response(['verify' => $valid], 200, [], 'json')->cacheControl('no-store');
+            } else {
+                $this->error('验证码错误');
+            }
+        }
         if(\think\Config::get('captcha.zh')) {
             $builder = new CaptchaZh();
         } else {
             $builder = new CaptchaBuilder();
         }
+        $builder->build();
         $timeout = config('captcha.timeout', 300);
         session('captcha_phrase', $builder->getPhrase());
         session('captcha_timeout', request()->time() . $timeout);
@@ -21,6 +33,7 @@ class Captcha {
             $verify = md5($builder->getPhrase());
             return response(['image' => $base64, 'verify' => $verify], 200, [], 'json')->cacheControl('no-store');
         } else {
+            $builder->build();
             $image = $builder->get();
             return response($image, 200)->contentType('image/jpeg')->cacheControl('no-store');
         }
